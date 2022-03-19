416793dbfd7e96cd41822bbcea32c79e28990c29
GabrielMarmore <gabrielpiraja@hotmail.com>
2022-03-18 22:27:49 -0300

    2 files modified

--- src/app/juros/juros.component.ts
+++ src/app/juros/juros.component.ts
@@ -9,16 +9,22 @@ export class JurosComponent implements OnInit {
   vp: number;
   j: number;
   n: number;
+  valores: Array<Number>;
 
   constructor() {
     this.vp = 100;
     this.j = 2.5;
     this.n = 3;
+    this.valores = [];
   }
 
   ngOnInit() {}
 
   getJuros() {
-    return this.vp * Math.pow(1 + this.j / 100, this.n);
+    for (var c = 0; c < this.n; c++) {
+      this.valores.push(this.vp * Math.pow(1 + this.j / 100, c));
+    }
+    return this.valores;
+    //return this.vp * Math.pow(1 + this.j / 100, this.n);
   }
 }
